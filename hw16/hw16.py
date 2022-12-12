16a765a18245c1a8e29171925487e6f46b9f0e9d
yarohkin <yarohkin255@ukr.net>
2022-12-13 00:01:19 +0200

    add class and static method

--- hw16/hw16.py
+++ hw16/hw16.py
@@ -4,3 +4,26 @@
 # Необязательное
 # Также класс должен считать количество подсчетов температуры и возвращать это значение с помощью статического метода.
 
+cel = int(input("Введите градусі в цельсиях: "))
+far = int(input("Введите градусі в фаренгейтах: "))
+
+class Temperature:
+    def __init__(self, celcii, farengeit):
+        self.celcii = celcii
+        self.farengeit = farengeit
+
+    @staticmethod
+    def Celcii():
+        celcii = cel
+        return celcii * 9 / 5 + 32
+
+    @staticmethod
+    def Farengeit():
+        farengeit = far
+        return (farengeit - 32) * 5 / 9
+
+t = Temperature(cel,far)
+tem = t.Celcii()
+tem1 = t.Farengeit()
+print(tem)
+print(tem1)
